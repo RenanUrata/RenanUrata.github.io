69c72d51a80cc35b4911fa7012542b9c195f0927
Renan Urata <renan.urata@gmail.com>
2021-10-11 22:35:51 -0300

    Portifolio Update

--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
+const bullseye = document.querySelector(".bullseye")
+const menu = document.querySelector(".menu")
+const particlesContainer = document.querySelector("#particles-js")
+const card = document.querySelector(".card")
+const front = document.querySelector(".front")
+const back = document.querySelector(".back")
+
+const showmenu = () => {
+	menu.classList.add("visible")
+	bullseye.classList.add("invisible")
+}
+const hidemenu = () => {
+	menu.classList.remove("visible")
+	bullseye.classList.remove("invisible")
+}
+const flipCard = () => {
+	front.classList.toggle('hideFront')
+	back.classList.toggle('showBack')
+}
+
+bullseye.addEventListener('click', () => { showmenu(); flipCard() })
+particlesContainer.addEventListener('click', () => { hidemenu(); flipCard() })
+
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', './particles.json', function() {
+particlesJS.load('particles-js', './particles.json', function () {
 	console.log('callback - particles.js config loaded')
 })
+
